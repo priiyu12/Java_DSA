a26132dac8c49b70b27eaabf8881f8e6aa983a8d
priiyu12 <136467028+priiyu12@users.noreply.github.com>
2024-03-28 15:57:15 +0530

    Create Q1.java

---
+++ Unit-1/Assignment/Q1.java
@@ -0,0 +1,13 @@
+//Write a java code to create an array of five elements and print on terminal
+
+public class PrintArray {
+  public static void main(String[] args) {
+    int[] arr = new int[5]; //declared array of size 5
+    arr = new int[]{8, 72, 15, 4, 42}; //initialized array with 5 values
+
+    //printing elements of an array
+    for(int val : arr){
+        System.out.println(val);
+    }
+  }
+}
